d2f8bd408d532277bcd921a28a94181e38e82809
HoneyDhiman6667 <honeydhiman235@gmail.com>
2025-05-28 23:27:28 +0530

    Working_Fine

---
+++ ASSIGNMENT 1 PY.py
@@ -0,0 +1,18 @@
+from operator import concat
+
+a= input('Enter the First number: ')
+b= input('Enter the Second number: ')
+a=int(a)
+b=int(b)
+
+print('Addition: ',(a+b))
+print('Subtraction: ',(a-b))
+print('Multiplication: ',(a*b))
+print('Division: ',(a/b))
+
+
+#TASK 2
+a=input("Enter the First name: ")
+b= input("Enter the second name: ")
+c= a+" "+b
+print(f"Hello,{c}! Welcome to the Python program.")
